56d9e4b769f0436c53bc885f23edafa3439b533f
Azeem Malik <at2244661122@gmail.com>
2026-07-20 02:39:41 -0700

    ✨ Render cart items dynamically in sidebar

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ const products = [
   },
 ];
 
+let cart = [];
+
 const productContainer = document.querySelector("#productContainer");
 
 function renderProducts() {
@@ -35,9 +37,11 @@ function renderProducts() {
       <div class="card">
         <img src="${product.image}" alt="${product.name}" />
 
-        <a href="#" class="pick-btn">
-          Add to Cart
-        </a>
+        <button
+         class="pick-btn add-to-cart"
+         data-id="${product.id}">
+         Add to Cart
+        </button>
       </div>
     `;
   });
@@ -45,9 +49,60 @@ function renderProducts() {
 
 renderProducts();
 
+function renderCart() {
+  cartItems.innerHTML = "";
+
+  cart.forEach((product) => {
+    cartItems.innerHTML += `
+    
+      <div class="cart-item">
+
+        <img src="${product.image}" alt="${product.name}">
+
+        <div class="cart-info">
+
+          <h5>${product.name}</h5>
+
+          <p>$${product.price}</p>
+
+        </div>
+
+      </div>
+
+    `;
+  });
+}
+
+setupCartButtons();
+
+function setupCartButtons() {
+  const buttons = document.querySelectorAll(".add-to-cart");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = Number(button.dataset.id);
+
+      const product = products.find((item) => {
+        return item.id === productId;
+      });
+
+      cart.push(product);
+
+      renderCart();
+
+      console.log(cart);
+    });
+  });
+}
+
 const hamburger = document.querySelector("#hamburger");
 const mobileMenu = document.querySelector("#mobileMenu");
 const icon = document.querySelector("#hamburger i");
+const cartSidebar = document.querySelector("#cartSidebar");
+const cartButton = document.querySelector("#cartButton");
+const mobileCartButton = document.querySelector("#mobileCartButton");
+const closeCart = document.querySelector("#closeCart");
+const cartItems = document.querySelector("#cartItems");
 
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("mobile-list-active");
@@ -75,4 +130,20 @@ window.addEventListener("scroll", () => {
   } else {
     document.body.classList.remove("scrolled");
   }
+});
+
+cartButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  cartSidebar.classList.add("cart-sidebar-active");
+});
+
+mobileCartButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  cartSidebar.classList.add("cart-sidebar-active");
+});
+
+closeCart.addEventListener("click", () => {
+  cartSidebar.classList.remove("cart-sidebar-active");
 });
