918fdaa1b61f891aa07d39bc581de24adc6b7c6d
Azeem Malik <at2244661122@gmail.com>
2026-07-20 02:03:25 -0700

    ✨ Render product cards dynamically with JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const products = [
+  {
+    id: 1,
+    name: "Velocity Green",
+    price: 129,
+    image: "./assets/shoe1.png",
+  },
+  {
+    id: 2,
+    name: "Shadow Runner",
+    price: 149,
+    image: "./assets/shoe2.png",
+  },
+  {
+    id: 3,
+    name: "Urban Motion",
+    price: 139,
+    image: "./assets/shoe3.png",
+  },
+  {
+    id: 4,
+    name: "Sprint Elite",
+    price: 159,
+    image: "./assets/shoe4.png",
+  },
+];
+
+const productContainer = document.querySelector("#productContainer");
+
+function renderProducts() {
+  productContainer.innerHTML = "";
+
+  products.forEach((product) => {
+    productContainer.innerHTML += `
+      <div class="card">
+        <img src="${product.image}" alt="${product.name}" />
+
+        <a href="#" class="pick-btn">
+          Add to Cart
+        </a>
+      </div>
+    `;
+  });
+}
+
+renderProducts();
+
 const hamburger = document.querySelector("#hamburger");
 const mobileMenu = document.querySelector("#mobileMenu");
 const icon = document.querySelector("#hamburger i");
